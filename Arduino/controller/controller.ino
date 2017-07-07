9ffa544c619c0920452d1e7864f7467b9a73e3c9
millerid <millerid>
2017-07-07 16:14:03 -0400

    Incremental: switching to new RFM69 library

--- Arduino/controller/controller.ino
+++ Arduino/controller/controller.ino
@@ -1,49 +1,90 @@
-#include <RFM69_ATC.h>
+#include <plainRFM69.h>
 //915MHz in the US
-#define FREQUENCY RF69_915MHZ
-//Network all robots must be on
-#define NETWORK_ID 100
-
-//The noise floor for automatic transmit power adjustment
-#define ATC_RSSI -80
+#define FREQUENCY 915
 
 //ID number of control radio
 #define CONTROLLER_ID 1
 
-#define MAX_CMD_LENGTH 50
+#define MAX_PACKET_LENGTH 64
 
-//We'll use ATC to save some battery, adjust power output automatically
-RFM69_ATC radio(10,0,false,0);
+// slave select pin.
+#define SLAVE_SELECT_PIN 10     
+
+// connected to the reset pin of the RFM69.
+#define RESET_PIN 1
+
+// Pin DIO 2 on the RFM69 is attached to this digital pin.
+#define DIO2_PIN 0
 
 //Buffer to hold command to send
-char Command[MAX_CMD_LENGTH];
+char Command[MAX_PACKET_LENGTH];
+char ReceiveBuffer[MAX_PACKET_LENGTH];
 int CommandLength;
+
+//Radio object
+plainRFM69 rfm = plainRFM69(SLAVE_SELECT_PIN);
 
 void setup() {
   //Setup serial connection to computer
   Serial.begin(115200);
   CommandLength = 0;
   
-  //Setup wireless
-  radio.initialize(FREQUENCY, CONTROLLER_ID, NETWORK_ID);
-  radio.setHighPower(); //Needed for HCW models
-  radio.enableAutoPower(ATC_RSSI); //Throttle transmit power automatically
+  SPI.begin();
+
+  bareRFM69::reset(RESET_PIN); // sent the RFM69 a hard-reset.
+
+  rfm.setRecommended();
+  //We have variable length packets, and want to use addressing.
+  rfm.setPacketType(true, true);
+  rfm.setNodeAddress(CONTROLLER_ID);
+
+  // allocate buffer in the library for received packets
+  rfm.setBufferSize(10);      // allow buffering of up to ten packets.
+  rfm.setPacketLength(64);    // maximum length of packets.
+
+  rfm.setFrequency((uint32_t) FREQUENCY*1000*1000); // set frequency to 915 MHz.
+  rfm.baud300000(); // Set the baudRate to 300000 bps
+
+  //Enable high power transmit
+  rfm.setPALevel(RFM69_PA_LEVEL_PA1_ON | RFM69_PA_LEVEL_PA2_ON, 31); 
+  rfm.setOCP(0);
+  rfm.setPa13dBm1(true);
+  rfm.setPa13dBm2(true);
+
+  // At higher packetrates it is necessary to increase this in order to ensure
+  // packet detection at the receiving side.
+  rfm.setPreambleSize(5); 
+
+  // tell the RFM to represent whether we are in automode on DIO 2.
+  rfm.setDioMapping1(RFM69_PACKET_DIO_2_AUTOMODE);
+
+  // set pinmode to input.
+  pinMode(DIO2_PIN, INPUT);
+
+  // Tell the SPI library we're going to use the SPI bus from an interrupt.
+  SPI.usingInterrupt(DIO2_PIN);
+
+  // hook our interrupt function to any edge.
+  attachInterrupt(DIO2_PIN, interrupt_RFM, CHANGE);
+
+  // start receiving.
+  rfm.receive();
+}
+
+//Interrupt handler for the radio
+void interrupt_RFM(){
+    rfm.poll(); // in the interrupt, call the poll function.
 }
 
 void loop() {
   //Do we have any new packets to process?
-  if (radio.receiveDone()) {
-    Serial.print((char)(radio.DATALEN+2));
-    Serial.print((char)radio.SENDERID);
-    for (int i=0; i<radio.DATALEN; i++) {
-      Serial.print((char)radio.DATA[i]);
-    }
-
-    //Send confirmation of reception if the sender asked
-    if (radio.ACKRequested())
-    {
-      byte theNodeID = radio.SENDERID;
-      radio.sendACK();
+  if (rfm.available()) {
+    char msgLength = rfm.read(&ReceiveBuffer);
+    
+    Serial.print((char)(msgLength+1));
+    //First byte is the target address, which we don't need
+    for (int i=1; i<msgLength; i++) {
+      Serial.print(ReceiveBuffer[i]);
     }
   }
 
@@ -55,7 +96,7 @@ void loop() {
     if (CommandLength==Command[0]) {
       //We have reached the end of the command
       //Second char is the address, rest is the payload
-      radio.send(Command[1], &(Command[2]), CommandLength-2, false);
+      rfm.sendAddressedVariable(Command[1], &(Command[2]), CommandLength-2);
       
       CommandLength = 0;
     }
--- Arduino/swarm/wirelessDriver.ino
+++ Arduino/swarm/wirelessDriver.ino
@@ -1,8 +1,8 @@
 #include "comm_protocol.h"
-#include <RFM69_ATC.h>
+#include <plainRFM69.h>
 
 //915MHz in the US
-#define FREQUENCY RF69_915MHZ
+#define FREQUENCY 915
 //Network all robots must be on
 #define NETWORK_ID 100
 
@@ -12,48 +12,100 @@
 //ID number of control radio
 #define CONTROLLER_ID 1
 
-//This is limited to 61 by the rfm61 library, choose lower value so we have space for headers and such
-#define MAX_PACKET_LENGTH 55
+#define MAX_PACKET_LENGTH 64
 
-//We'll use ATC to save some battery, adjust power output automatically
-RFM69_ATC radio(10, 0, false, 0);
+// slave select pin.
+#define SLAVE_SELECT_PIN 10     
+
+// connected to the reset pin of the RFM69.
+#define RESET_PIN 1
+
+// Pin DIO 2 on the RFM69 is attached to this digital pin.
+#define DIO2_PIN 0
+
+//Receive Buffer
+char ReceiveBuffer[MAX_PACKET_LENGTH];
+//Radio object
+plainRFM69 rfm = plainRFM69(SLAVE_SELECT_PIN);
 
 void setupWireless() {
-  radio.initialize(FREQUENCY, ConstData.NodeID, NETWORK_ID);
-  radio.setHighPower(); //Needed for HCW models
-  radio.enableAutoPower(ATC_RSSI); //Throttle transmit power automatically
+  SPI.begin();
+
+  bareRFM69::reset(RESET_PIN); // sent the RFM69 a hard-reset.
+
+  rfm.setRecommended();
+  //We have variable length packets, and want to use addressing.
+  rfm.setPacketType(true, true);
+  rfm.setNodeAddress(ConstData.NodeID);
+
+  // allocate buffer in the library for received packets
+  rfm.setBufferSize(10);      // allow buffering of up to ten packets.
+  rfm.setPacketLength(64);    // maximum length of packets.
+
+  rfm.setFrequency((uint32_t) FREQUENCY*1000*1000); // set frequency to 915 MHz.
+  rfm.baud300000(); // Set the baudRate to 300000 bps
+
+  //Enable high power transmit
+  rfm.setPALevel(RFM69_PA_LEVEL_PA1_ON | RFM69_PA_LEVEL_PA2_ON, 31); 
+  rfm.setOCP(0);
+  rfm.setPa13dBm1(true);
+  rfm.setPa13dBm2(true);
+
+  // At higher packetrates it is necessary to increase this in order to ensure
+  // packet detection at the receiving side.
+  rfm.setPreambleSize(5); 
+
+  // tell the RFM to represent whether we are in automode on DIO 2.
+  rfm.setDioMapping1(RFM69_PACKET_DIO_2_AUTOMODE);
+
+  // set pinmode to input.
+  pinMode(DIO2_PIN, INPUT);
+
+  // Tell the SPI library we're going to use the SPI bus from an interrupt.
+  SPI.usingInterrupt(DIO2_PIN);
+
+  // hook our interrupt function to any edge.
+  attachInterrupt(DIO2_PIN, interrupt_RFM, CHANGE);
+
+  // start receiving.
+  rfm.receive();
+}
+
+//Interrupt handler for the radio
+void interrupt_RFM(){
+    rfm.poll(); // in the interrupt, call the poll function.
 }
 
 void checkForCommands() {
   //Do we have any new packets to process?
-  if (radio.receiveDone()) {
-    switch (radio.DATA[0]) {
+  if (rfm.available()) {
+    char msgLength = rfm.read(&ReceiveBuffer);
+    //First byte is the target address, which we don't need
+    switch (ReceiveBuffer[1]) {
       case DUMP:
         //Send everything except the checksum
-        sendResponse(radio.SENDERID, DUMP, (char*)&ConstData, sizeof(ConstData));
+        sendResponse(254, DUMP, (char*)&ConstData, sizeof(ConstData));
         break;
       case SET_CONSTS:
         //Ok, in that case the rest of the packet is the ConstData struct
         //Make sure it isn't corrupted or weird somehow
-        if (radio.DATALEN-1 == sizeof(ConstData)) {
+        if (msgLength-1 == sizeof(ConstData)) {
           //A bit sketchy, since radio.DATA is technically volatile.
           //Copy radio data into our struct
-          memcpy(&ConstData, (char *)&(radio.DATA[1]), sizeof(ConstData));
+          memcpy(&ConstData, (char *)&(ReceiveBuffer[1]), sizeof(ConstData));
           //Save new values to EEPROM
           saveEepromData();
           //Apply the new ID
-          radio.setAddress(ConstData.NodeID);
-        } else {
-          sendResponse(radio.SENDERID, SET_CONSTS, (char *)&radio.DATALEN, sizeof(radio.DATALEN));
+          rfm.setNodeAddress(ConstData.NodeID);
         }
         break;
       case DRIVE:
         //Make sure our packet is max gucci
-        if (radio.DATALEN-1 == sizeof(DRIVE_ARGS)) {
+        if (msgLength-1 == sizeof(DRIVE_ARGS)) {
           ManualMode = true;
           //Cast arguments into struct for more easy access
           DRIVE_ARGS * args; 
-          args = (DRIVE_ARGS*)&(radio.DATA[1]);
+          args = (DRIVE_ARGS*)&(ReceiveBuffer[1]);
   
           Serial.print("driving: ");
           Serial.println(args->RSpeed);
@@ -84,12 +136,12 @@ void sendResponse(int targetId, char cmd, void * payload, int payloadLength) {
     fullPayload[i+2] = *((char*)payload+i);
   }
   //Send the assembled packet
-  radio.send(targetId, fullPayload, payloadLength+2, false);
+  rfm.sendAddressedVariable(targetId, fullPayload, payloadLength+2);
 }
 
 //Used to push updates to the controller
 void sendStatusUpdate(int targetId, char updateType, void * payload, int payloadLength) {
-  char packetNumber = 0;
+  /*char packetNumber = 0;
   char numPackets = payloadLength/MAX_PACKET_LENGTH;
   int packetLength = 0;
   char fullPayload[MAX_PACKET_LENGTH+4];
@@ -110,6 +162,6 @@ void sendStatusUpdate(int targetId, char updateType, void * payload, int payload
     } else {
       Serial.println("packet sending failed, retry");
     }
-  } while (packetLength == MAX_PACKET_LENGTH); //Keep going while we have more to transmit
+  } while (packetLength == MAX_PACKET_LENGTH); //Keep going while we have more to transmit*/
 }
 
