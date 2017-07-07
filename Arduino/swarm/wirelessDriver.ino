0755c5bba6e96497bd09ad92543a8a20727ac58a
millerid <millerid>
2017-07-07 16:59:36 -0400

    switched to new rfm69 library mostly complete

--- Arduino/swarm/wirelessDriver.ino
+++ Arduino/swarm/wirelessDriver.ino
@@ -80,19 +80,19 @@ void checkForCommands() {
   //Do we have any new packets to process?
   if (rfm.available()) {
     char msgLength = rfm.read(&ReceiveBuffer);
+    char sender = ReceiveBuffer[1];
     //First byte is the target address, which we don't need
-    switch (ReceiveBuffer[1]) {
+    switch (ReceiveBuffer[2]) {
       case DUMP:
         //Send everything except the checksum
-        sendResponse(254, DUMP, (char*)&ConstData, sizeof(ConstData));
+        sendResponse(sender, DUMP, (char*)&ConstData, sizeof(ConstData));
         break;
       case SET_CONSTS:
         //Ok, in that case the rest of the packet is the ConstData struct
         //Make sure it isn't corrupted or weird somehow
-        if (msgLength-1 == sizeof(ConstData)) {
-          //A bit sketchy, since radio.DATA is technically volatile.
+        if (msgLength-3 == sizeof(ConstData)) {
           //Copy radio data into our struct
-          memcpy(&ConstData, (char *)&(ReceiveBuffer[1]), sizeof(ConstData));
+          memcpy(&ConstData, (char *)&(ReceiveBuffer[3]), sizeof(ConstData));
           //Save new values to EEPROM
           saveEepromData();
           //Apply the new ID
@@ -101,11 +101,11 @@ void checkForCommands() {
         break;
       case DRIVE:
         //Make sure our packet is max gucci
-        if (msgLength-1 == sizeof(DRIVE_ARGS)) {
+        if (msgLength-3 == sizeof(DRIVE_ARGS)) {
           ManualMode = true;
           //Cast arguments into struct for more easy access
           DRIVE_ARGS * args; 
-          args = (DRIVE_ARGS*)&(ReceiveBuffer[1]);
+          args = (DRIVE_ARGS*)&(ReceiveBuffer[3]);
   
           Serial.print("driving: ");
           Serial.println(args->RSpeed);
@@ -130,38 +130,42 @@ void sendResponse(int targetId, char cmd, void * payload, int payloadLength) {
   Serial.println(payloadLength);
   //Prepend response header to payload
   char fullPayload[payloadLength+2];
-  fullPayload[0] = REPLY_SIGNAL;
-  fullPayload[1] = cmd;
+  fullPayload[0] = ConstData.NodeID;
+  fullPayload[1] = REPLY_SIGNAL;
+  fullPayload[2] = cmd;
   for (int i=0; i<payloadLength; i++) {
-    fullPayload[i+2] = *((char*)payload+i);
+    fullPayload[i+3] = *((char*)payload+i);
   }
   //Send the assembled packet
-  rfm.sendAddressedVariable(targetId, fullPayload, payloadLength+2);
+  rfm.sendAddressedVariable(targetId, fullPayload, payloadLength+3);
 }
 
 //Used to push updates to the controller
 void sendStatusUpdate(int targetId, char updateType, void * payload, int payloadLength) {
-  /*char packetNumber = 0;
+  Serial.println("sending status update...");
+  
+  char packetNumber = 0;
   char numPackets = payloadLength/MAX_PACKET_LENGTH;
   int packetLength = 0;
-  char fullPayload[MAX_PACKET_LENGTH+4];
+  char fullPayload[MAX_PACKET_LENGTH+5];
 
   do {
     //Prepend update header to payload
-    packetLength = min((payloadLength)-(packetNumber*MAX_PACKET_LENGTH), MAX_PACKET_LENGTH);
-    fullPayload[0] = STATUS_SIGNAL;
-    fullPayload[1] = updateType;
-    fullPayload[2] = packetNumber;
-    fullPayload[3] = numPackets;
+    packetLength = min((payloadLength)-(packetNumber*MAX_PACKET_LENGTH), MAX_PACKET_LENGTH)-5;
+    fullPayload[0] = ConstData.NodeID;
+    fullPayload[1] = STATUS_SIGNAL;
+    fullPayload[2] = updateType;
+    fullPayload[3] = packetNumber;
+    fullPayload[4] = numPackets;
     for (int i=0; i<packetLength; i++) {
-      fullPayload[i+4] = *((char*)payload+i+(packetNumber*MAX_PACKET_LENGTH));
+      fullPayload[i+5] = *((char*)payload+i+(packetNumber*MAX_PACKET_LENGTH));
     }
-    //Send the assembled packet
-    if (radio.sendWithRetry(targetId, fullPayload, packetLength+4)) {
+
+    if (rfm.canSend()) {
+      Serial.println(packetLength+5);
+      rfm.sendAddressedVariable(targetId, fullPayload, packetLength+5);
       packetNumber++;
-    } else {
-      Serial.println("packet sending failed, retry");
     }
-  } while (packetLength == MAX_PACKET_LENGTH); //Keep going while we have more to transmit*/
+  } while (packetNumber <= numPackets); //Keep going while we have more to transmit
 }
 
--- Docs/wireless.md
+++ Docs/wireless.md
@@ -2,21 +2,24 @@
 
 ## Command format
 - Command structured as series of unsigned chars
+  - Prefaced by sender ID
   - Commands can only be sent by controller
   - Command (and args) a single char as defined by an enum (starting with 0x10)
   - Characters below 0x10 are reserved for special use
     -0x00 indicates a reply, 0x01 a status update, 0x0A is reserved for newline applications
-  - To drive straight forwards (drive is 0x08), then, would be `15 FF FF`
+  - To drive straight forwards (drive is 0x08), then, would be `01 15 FF FF` (01 is the sender)
 
 - Responses
+  - Prefaced by sender ID
   - Can only be sent by nodes
   - Responses encode the original command.
-  - 0x00 signals a response, followed by the original command, and then the payload
-  - Response to dump (0x10) would be `00 10 <data>`
+  - 0x00 signals a response, , followed by the original command, and then the payload
+  - Response to dump (0x10) would be `FE 00 10 <data>` (FE is the sender)
 
 - Updates
+  - Prefaced by sender ID
   - 0x01 signals a status update, followed by the update type, then by the packet number, then the expected number of packets
-  - A target list update might then be `01 10 00 03 ...`
+  - A target list update might then be `FE 01 10 00 03 ...`
     - This packet would be the 1st of 4 packets (indexing starts from 0)
 
 ## Command list
