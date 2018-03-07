Added comparison with minimization algorithm in real time.  Lots of bug fixes in minimization algorithm and kalman filter models having to do with coordinate system problems (particularly signs of angles)

Controller/motioncapture.py
@@ -4,6 +4,7 @@ from kivy.graphics.texture import Texture
 
 from constants import *
 from nodesensordisplay import NodeSensorDisplay
+from scipy.optimize import minimize
 
 import csv
 import math
@@ -21,6 +22,7 @@ class MotionCapture(Image, NodeSensorDisplay):
     def __init__(self, **kwargs):
         super(MotionCapture, self).__init__(**kwargs)
         self.logging = False
+        self.min_pts = np.zeros(0)
         #load calibration constants for camera
         self.camera_calib_mtx = np.load('camera_calib_mtx.npy')
         self.camera_calib_dist = np.load('camera_calib_dist.npy')
@@ -60,13 +62,14 @@ class MotionCapture(Image, NodeSensorDisplay):
             self.node_list.append(node['data'])
         #print self.node_list
         
-    def draw_robot(self, pos, color, frame):
+    def draw_robot(self, pos, color, num, frame):
         #project lines of detected robot locations onto the screen for rendering
         pts, jac = cv2.projectPoints(robot_arrow, np.array([0,0,pos[2]], np.float32),
                                      np.array([pos[0],pos[1],220], np.float32),
                                      self.camera_calib_mtx, self.camera_calib_dist)
         #draw robots on screen
         frame = cv2.arrowedLine(frame, tuple(pts[0].ravel()), tuple(pts[1].ravel()), color, 5, tipLength=0.3)
+        cv2.putText(frame, str(num), tuple(pts[0].ravel()), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=3)
         
     def track_robots(self, frame):
         #detect the corners and ids of all the aruco markers
@@ -111,26 +114,39 @@ class MotionCapture(Image, NodeSensorDisplay):
             #detect robots
             measured_pos = self.track_robots(frame)
                 
-            #draw calculated robot positions
+            #get calculated robot positions
             calculated_pos = np.zeros([len(self.node_list), 3])
+            
             for n in self.node_list:
                 #expects nodes to be numbered 2,3,4...
                 calculated_pos[n.node_id-2] = [n.pos[0]/5., n.pos[1]/5., -np.radians(n.angle)]
-            
+                
+            min_pts = np.copy(self.min_pts)
             if (len(calculated_pos) == len(measured_pos)): 
                 #fit points onto eachother
-                err = self.normalize_pts(calculated_pos, measured_pos)
-                if (self.logging):
-                    #find difference b/w known and calculated states
-                    difference = calculated_pos-measured_pos
-                    #log difference data to csv (like a boss)
-                    self.csv_writer.writerow(difference.flatten())
+                err1 = self.normalize_pts(calculated_pos, measured_pos)
+                if (len(measured_pos) == len(self.min_pts)):
+                    err2 = self.normalize_pts(min_pts, measured_pos)
+                    if (self.logging):
+                        #find difference b/w known and calculated states
+                        difference_filter = calculated_pos-measured_pos
+                        difference_min = min_pts-measured_pos
+                        #log difference data to csv (like a boss)
+                        self.csv_writer.writerow(np.append(difference_filter.flatten(),difference_min.flatten()))
                 
             #actually draw stuff
+            i = 0
             for p in calculated_pos:
-                self.draw_robot(p, (255,0,0), frame)
+                self.draw_robot(p, (255,0,0), i, frame)
+                i += 1
+            i = 0
             for p in measured_pos:   
-                self.draw_robot(p, (0,255,0), frame)
+                self.draw_robot(p, (0,255,0), i, frame)
+                i += 1
+            i=0
+            for p in min_pts:   
+                self.draw_robot(p, (0,0,255), i, frame)
+                i += 1
             
             # convert OpenCV image to Kivy texture
             buf1 = cv2.flip(frame, 0)
@@ -139,3 +155,20 @@ class MotionCapture(Image, NodeSensorDisplay):
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
+            
+    #run minimization algorithm when we get new data 
+    #comment out if speed is desired       
+    def trigger_new_data(self):
+        Dp = self.gen_adjacencies()
+
+        if (Dp.any()):
+            # try to minimize error
+            x0 = np.zeros((Dp.shape[0], 3))
+            #args has to be a tuple, because of weird numpy problems
+            out = minimize(fun=self.sum_errors, x0=x0, args=(Dp,), method='SLSQP')
+            # format stuff nicely and output
+            self.min_pts = np.reshape(out.x, (out.x.shape[0]/3, 3))
+            #scale things to be in the right coordinate system
+            self.min_pts[:,2] *= -1
+            self.min_pts[:,0:2] /= 5.
+            

Controller/node.py
@@ -27,12 +27,12 @@ def move(x, u, dt, l):
     else:
         Vl = 0
     if (u[1] != 0):    
-        Vr = 1024//abs(int(u[1]))*np.sign(u[0])
+        Vr = 1024//abs(int(u[1]))*np.sign(u[1])
         Vr = 1024.0/Vr
     else:
         Vr = 0
         
-    print str(Vl) + "  " + str(Vr)
+    #print str(Vl) + "  " + str(Vr)
     
     #scale appropriately to screen
     Vl = 0.2*Vl
@@ -40,13 +40,13 @@ def move(x, u, dt, l):
 
     #special case for straight motion
     if (Vl == Vr):
-        return np.array([x[0]+Vl*dt*np.cos(x[2]), x[1]+Vl*dt*np.sin(x[2]), x[2]])
+        return np.array([x[0]+Vl*dt*np.cos(-x[2]), x[1]+Vl*dt*np.sin(-x[2]), x[2]])
     
     R = (l/4)*(Vl+Vr)/(Vl-Vr)
     omega = (Vl-Vr)/l
     
     #calculate ICC position
-    ICC = np.array([x[0]-R*np.sin(x[2]), x[1]+R*np.cos(x[2])])
+    ICC = np.array([x[0]-R*np.sin(-x[2]), x[1]+R*np.cos(-x[2])])
 
     #calculate the new transformed position
     rotation = np.array([[np.cos(omega*dt), -np.sin(omega*dt), 0],
@@ -87,7 +87,7 @@ def Hx(x, landmarks):
     for lmark in landmarks:
         px, py = lmark
         dist = np.sqrt((px - x[0])**2 + (py - x[1])**2)
-        angle = np.arctan2(py - x[1], px - x[0])
+        angle = np.arctan2(x[1] - py, px - x[0])
         hx.extend([dist, normalize_angle(angle - x[2])])
     return np.array(hx)
     
@@ -251,7 +251,7 @@ class Node(Widget):
                                     subtract=residual_x)
 
         self.ukf = UKF(dim_x=3, fx=fx, hx=Hx,
-                  dt=1./60, points=points, x_mean_fn=state_mean, 
+                  dt=1./30, points=points, x_mean_fn=state_mean, 
                   z_mean_fn=z_mean, residual_x=residual_x, 
                   residual_z=residual_h)
         
@@ -260,7 +260,7 @@ class Node(Widget):
         self.ukf.P = np.diag([5, 5, 0.1])
         #sensor noise
         self.ukf.R = np.array([10**2, 
-                         0.05**2])
+                         0.1**2])
         #process noise                 
         self.ukf.Q = np.diag([0.01, 0.01, 0.01])
         
@@ -268,14 +268,14 @@ class Node(Widget):
         self.ukf_predict(0.0001)
         
     def ukf_predict(self, dt):
-        try:
-            self.state = fx(self.state, dt, self.control)
-        except AttributeError:
-            self.state = [self.pos[0], self.pos[1], np.radians(self.angle)]
+        #try:
+        #    self.state = fx(self.state, dt, self.control)
+        #except AttributeError:
+        #    self.state = [self.pos[0], self.pos[1], np.radians(self.angle)]
             
-        self.pos[0] = int(self.state[0])
-        self.pos[1] = int(self.state[1])
-        self.angle = int(np.degrees(self.state[2]))
+        #self.pos[0] = int(self.state[0])
+        #self.pos[1] = int(self.state[1])
+        #self.angle = int(np.degrees(self.state[2]))
         
         self.ukf.predict(fx_args=self.control, dt=dt)
         
@@ -285,6 +285,7 @@ class Node(Widget):
         
     def ukf_update(self, z, l_pos):
         self.ukf.update(z, hx_args=l_pos,)
+        #print self.ukf.P
 
     def process_targets(self):
         #figure out force vector

Controller/nodesensordisplay.py
@@ -6,6 +6,7 @@ from constants import *
 import math
 import numpy as np
 from scipy.optimize import minimize
+import rmsd
 
 class NodeSensorDisplay(Widget):
     def __init__(self, **kwargs):
@@ -16,6 +17,7 @@ class NodeSensorDisplay(Widget):
         
     # process points to fit pts1 onto pts2
     # uses algorithm described here: http://nghiaho.com/?page_id=671
+    # also https://en.wikipedia.org/wiki/Kabsch_algorithm
     def normalize_pts(self, pts1, pts2):
         #print pts1
         A = pts1[:, 0:2]
@@ -26,41 +28,23 @@ class NodeSensorDisplay(Widget):
         N = A.shape[0]  # total points
 
         # find centroids
-        centroid_A = np.mean(A, axis=0)
-        centroid_B = np.mean(B, axis=0)
+        centroid_A = rmsd.centroid(A)
+        centroid_B = rmsd.centroid(B)
 
         # centre the points
-        AA = A - np.tile(centroid_A, (N, 1))
-        BB = B - np.tile(centroid_B, (N, 1))
-
-        H = np.dot(np.transpose(AA), BB)
-        U, S, Vt = np.linalg.svd(H)
-
-        # rotation matrix
-        R = np.dot(Vt.T, U.T)
-
-        # special reflection case
-        if np.linalg.det(R) > 0:
-            #print "Reflection detected"
-            Vt[1, :] *= -1
-            R = np.dot(Vt.T, U.T)
-
-        # calculate transposition
-        t = np.dot(-R, centroid_A.T) + centroid_B.T
-
+        AA = A - centroid_A
+        BB = B - centroid_B
+        
+        #get optimal rotation matrix
+        R = rmsd.kabsch(AA, BB)
         # Apply transformation
-        A2 = np.dot(R, A.T) + np.tile(t[:, None], (1, N))
-        A2 = A2.T
-
-        pts1[:, 0:2] = A2
+        AA1 = np.dot(AA, R)
+        angle1 = math.atan2(R[1][0], R[1][1])
+        
+        pts1[:, 0:2] = AA1 + centroid_B
         # rotate the rotations
-        pts1[:, 2] += math.atan2(R[0][1], R[0][0])
-
-        # calculate the error
-        err = A2 - B
-        err = np.multiply(err, err)
-        err = np.sum(err)
-        return err
+        pts1[:, 2] -= angle1
+        return rmsd.rmsd(AA1, BB)
 
     # p is of the following format
     # p[n][v]
@@ -86,7 +70,7 @@ class NodeSensorDisplay(Widget):
         #y-component
         D[:,:,1] = np.multiply(px_outer, sin_mat[:, np.newaxis])
         D[:,:,1] += np.multiply(py_outer, cos_mat[:, np.newaxis])
-
+        
         #remove data that we don't have sensor data for
         D = np.multiply(D, args[0].any(axis = 2)[:, :, np.newaxis])
         # Find all differences
@@ -116,17 +100,19 @@ class NodeSensorDisplay(Widget):
 
                 # format stuff nicely and output
                 self.pts = np.reshape(out.x, (out.x.shape[0]/3, 3))
-                if (hasattr(self, 'last_pts')):
+                
+                #print self.pts
+                
+                #if (hasattr(self, 'last_pts')):
                     #align points to be closest to the last
-                    self.normalize_pts(self.pts, self.last_pts)
+                #    self.normalize_pts(self.pts, self.last_pts)
 
                 #update nodes with postitions and angles
                 all_reports_heard = True
+                
                 for i in range(0, len(self.node_list)):
                     if (len(self.node_list[i].target_list) == 0):
                         all_reports_heard = False
-                    
-                    print self.pts[i]
                     
                     for n in self.node_list:
                         if (FREQUENCIES[n.freq] == i):
@@ -142,7 +128,7 @@ class NodeSensorDisplay(Widget):
                     for n in self.node_list:
                         n.ukf_init()
                     self.timer.cancel()
-                    self.timer = Clock.schedule_interval(self.update, 1./60)
+                    self.timer = Clock.schedule_interval(self.update, 1./30)
                     
                 self.last_pts = np.copy(self.pts)
         else:
@@ -161,3 +147,7 @@ class NodeSensorDisplay(Widget):
                 adj[FREQUENCIES[n.freq]][adj_n['bin']][0] = dist*np.cos(np.radians(adj_n['direction']))
                 adj[FREQUENCIES[n.freq]][adj_n['bin']][1] = dist*np.sin(np.radians(adj_n['direction']))
         return adj
+        
+    #placeholder, want to extend in subclass
+    def trigger_new_data(self):
+        pass

MathTesting/CalcPos.py
@@ -5,6 +5,7 @@ import random
 import math
 import timeit
 import csv
+import rmsd
 
 # Basically just a struct to store node data
 class Node:
@@ -40,18 +41,20 @@ def scan_for_neighbors(node, node_list):
     for n in node_list:
         if (n != node):
             dist = math.sqrt((n.pos[0]-node.pos[0])**2+(n.pos[1]-node.pos[1])**2)
-            angle = -(math.atan2(n.pos[1]-node.pos[1], n.pos[0]-node.pos[0])-math.radians(node.angle))
+            angle = (math.atan2(n.pos[1]-node.pos[1], n.pos[0]-node.pos[0])+math.radians(node.angle))
             # add error
-            #dist += np.random.normal(0, 10)
+            #dist += np.random.normal(0, 30)
             #angle += np.random.normal(0, 0.1)
-            if (dist < 100):
+            if (dist < 500):
                 list.append({'distance': dist, 'direction': angle, 'bin': n.id_num})
 
+    print list
     return list
 
 # process points to fit pts1 onto pts2
 # uses algorithm described here: http://nghiaho.com/?page_id=671
-def normalize_pts(pts1, pts2):
+def normalize_pts_old(pts1, pts2, win):
+    #print pts1
     A = pts1[:, 0:2]
     B = pts2[:, 0:2]
 
@@ -60,41 +63,87 @@ def normalize_pts(pts1, pts2):
     N = A.shape[0]  # total points
 
     # find centroids
-    centroid_A = np.mean(A, axis=0)
-    centroid_B = np.mean(B, axis=0)
+    centroid_A = rmsd.centroid(A)
+    centroid_B = rmsd.centroid(B)
 
     # centre the points
-    AA = A - np.tile(centroid_A, (N, 1))
-    BB = B - np.tile(centroid_B, (N, 1))
-
-    H = np.dot(np.transpose(AA), BB)
-    U, S, Vt = np.linalg.svd(H)
-
-    # rotation matrix
-    R = np.dot(Vt.T, U.T)
-
-    # special reflection case
-    if np.linalg.det(R) > 0:
-        print "Reflection detected"
-        Vt[1, :] *= -1
-        R = np.dot(Vt.T, U.T)
-
-    # calculate transposition
-    t = np.dot(-R, centroid_A.T) + centroid_B.T
-
+    AA = A - centroid_A
+    BB = B - centroid_B
+    
+    #get optimal rotation matrix
+    R = rmsd.kabsch(AA, BB)
     # Apply transformation
-    A2 = np.dot(R, A.T) + np.tile(t[:, None], (1, N))
-    A2 = A2.T
-
-    pts1[:, 0:2] = A2
+    AA1 = np.dot(AA, R)
+    angle1 = math.atan2(R[1][0], R[1][1])
+    
+    i=0
+    for pt in pts1:
+        pt_graphics = Point(int(pt[0]-centroid_A[0]+centroid_B[0]), int(pt[1]-centroid_A[1]+centroid_B[1]))
+        cir = Circle(pt_graphics, 8)
+        cir.setOutline('green')
+        cir.draw(win)
+        line = Line(pt_graphics, Point(pt_graphics.x+8*np.cos(-pt[2]), pt_graphics.y+8*np.sin(-pt[2])))
+        line.draw(win)
+        text = Text(pt_graphics, str(i))
+        i += 1
+        text.draw(win)
+    
+    pts1[:, 0:2] = AA1 + centroid_B
     # rotate the rotations
-    pts1[:, 2] += math.atan2(R[0][1], R[0][0])
+    pts1[:, 2] -= angle1
+    return rmsd.rmsd(AA1, BB)
 
-    # calculate the error
-    err = A2 - B
-    err = np.multiply(err, err)
-    err = np.sum(err)
-    return err
+def normalize_pts(pts1, pts2):
+    #print pts1
+    A = pts1[:, 0:2]
+    B = pts2[:, 0:2]
+
+    assert len(A) == len(B)
+
+    N = A.shape[0]  # total points
+
+    # find centroids
+    centroid_A = rmsd.centroid(A)
+    centroid_B = rmsd.centroid(B)
+
+    # centre the points
+    AA = A - centroid_A
+    BB = B - centroid_B
+    
+    #get optimal rotation matrix
+    R = rmsd.kabsch(AA, BB)
+    # Apply transformation
+    AA1 = np.dot(AA, R)
+    angle1 = math.atan2(R[1][0], R[1][1])
+    
+    #try flippin' it
+    AA[:, 1] *= -1
+    #get optimal rotation matrix
+    R = rmsd.kabsch(AA, BB)
+    # Apply transformation
+    AA2 = np.dot(AA, R)#get optimal rotation matrix
+    angle2 = math.atan2(R[1][0], R[1][1])
+    
+    #go with whichever one is better
+    a2diff = np.abs(pts2[:,2]-(pts1[:,2]-angle2))
+    a1diff = np.abs(pts2[:,2]-(pts1[:,2]+angle1))
+    #angle differences are fun!
+    for i in range(0, len(a1diff)):
+        while (a2diff[i] > np.pi):
+            a2diff[i] -= 2*np.pi
+        while (a1diff[i] > np.pi):
+            a1diff[i] -= 2*np.pi
+    print np.degrees(a2diff), np.degrees(a1diff)
+    if (np.sum(np.abs(a1diff)) > np.sum(np.abs(a2diff))):
+        pts1[:, 0:2] = AA2 + centroid_B
+        # rotate the rotations (backwards, since we mirrored the thing)
+        pts1[:, 2] -= angle2
+        return rmsd.rmsd(AA2, BB)
+    else:
+        pts1[:, 0:2] = AA1 + centroid_B
+        # rotate the rotations
+        pts1[:, 2] += angle1
+        return rmsd.rmsd(AA1, BB)
 
 # p is of the following format
 # p[n][v]
@@ -142,14 +191,17 @@ csvfile = open(name="data.csv", mode='wb')
 csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 csvwriter.writerow(['num', 'err_solve', 'error_abs', 'time'])
 
-random.seed(1)
+#uncomment for uniformity
+#random.seed(10)
 for i in range(0, 1):
     # generate random config
-    num = 20
+    num = 3
 
     print "iter "+str(i) + ": "+ str(num)+" nodes"
-
+    
     pts_act, Dp = gen_configuration(num)
+    
+    print Dp
 
     # try to minimize error
     x0 = np.zeros((Dp.shape[0], 3))
@@ -161,46 +213,64 @@ for i in range(0, 1):
     solve_time = timeit.default_timer() - start_time
 
     print solve_time
-
+    
     # format stuff nicely and output
     pts = np.reshape(out.x, (out.x.shape[0]/3, 3))
-    err = normalize_pts(pts, pts_act)
+    
+    '''
+    pts = np.array([[ 100+25.4,        100-20.2,          +1.60570291],
+                     [ 100-2.2,         100+51.4,         -0.78539816],
+                     [100-40.4,          100+8.,          -0.17453293]])
+    pts_act = np.array([[100+31.44893379, 100-30.22211118,   +1.13694827],
+                        [100-36.55305224, 100-28.97042214,  -2.87720222],
+                        [100-6.91792634, 100-75.77796166,  -1.86272542]])
+                        '''
+    win = GraphWin("plot", 500, 500)
+    
+    err = normalize_pts_old(pts,pts_act,win)
 
     # print out
     # print "=================================="
     # print pts
-    # print "Final error: "+str(out.fun)
-    # print "Actual error: "+str(err)
+    print "Final error: "+str(out.fun)
+    print "Actual error: "+str(err)
 
     #if we have a solver failure, say something!
-    if (err > 0.001):
+    '''if (err > 0.001):
         print out
         print "=================================="
         print pts
         print "Final error: "+str(out.fun)
-        print "Actual error: "+str(err)
+        print "Actual error: "+str(err)'''
 
-    csvwriter.writerow([num, out.fun, err, solve_time])
+    #csvwriter.writerow([num, out.fun, err, solve_time])
 
-csvfile.close()
+#csvfile.close()
 
 # draw calculated locations on screen
-win = GraphWin("plot", 500, 500)
-
+i=0
 for pt in pts:
     pt_graphics = Point(int(pt[0]), int(pt[1]))
     cir = Circle(pt_graphics, 8)
     cir.draw(win)
     line = Line(pt_graphics, Point(pt_graphics.x+8*np.cos(-pt[2]), pt_graphics.y+8*np.sin(-pt[2])))
     line.draw(win)
+    text = Text(pt_graphics, str(i))
+    i += 1
+    text.draw(win)
 
+i=0
 for pt in pts_act:
     pt_graphics = Point(int(pt[0]), int(pt[1]))
     cir = Circle(pt_graphics, 8)
     cir.setOutline('red')
     cir.draw(win)
     line = Line(pt_graphics, Point(pt_graphics.x+8*np.cos(-pt[2]), pt_graphics.y+8*np.sin(-pt[2])))
+    line.setOutline('red')
     line.draw(win)
+    text = Text(pt_graphics, str(i))
+    i += 1
+    text.draw(win)
 
 # wait before closing window
 win.getMouse()