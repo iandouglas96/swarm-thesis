eeab9c50ededd346c802bf78d0bc305eab067295
Ian D. Miller <iandouglas96@gmail.com>
2018-04-03 23:37:53 -0400

    Added several preset test cases

--- Controller/runpreset.py
+++ Controller/runpreset.py
@@ -22,11 +22,31 @@ class RunPreset(threading.Thread):
         print "Error, robot with id "+str(id_num)+" not found!"
     
     def formation1(self):
-        print "Running formation 1"
-        self.send_command(2, 100, 100)
-        time.sleep(5)
+        '''
+        print "Running formation 1: CCW circle"
+        self.send_command(2, 120, 100)
+        self.send_command(3, 120, 100)
+        self.send_command(4, 120, 100)
+        time.sleep(60)
         self.send_command(2, 0, 0)
+        self.send_command(3, 0, 0)
+        self.send_command(4, 0, 0)
         print "Formation 1 complete"
+        '''
+
+        print "Running formation 2: Star expand"
+        self.send_command(2, 100, 100)
+        self.send_command(3, 100, 100)
+        self.send_command(4, 100, 100)
+        time.sleep(10)
+        self.send_command(2, -100, -100)
+        self.send_command(3, -100, -100)
+        self.send_command(4, -100, -100)
+        time.sleep(10)
+        self.send_command(2, 0, 0)
+        self.send_command(3, 0, 0)
+        self.send_command(4, 0, 0)
+        print "Formation 2 complete"
 
     def run(self):
         #run through sequence of commands, then finish.
